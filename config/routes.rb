195d59156c105d431f8d9d183bf7e36dd12e7eeb
Rastakilla <rastakilla@lanjdl.ca>
2018-01-31 16:48:05 -0500

    google

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'visitors/index'
+  get "/pages/*id" => 'pages#show', as: :page, format: false
   root to: 'visitors#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
