a922e1999d366c07322278e007853355199039de
Rastakilla <rastakilla@lanjdl.ca>
2018-02-12 21:28:36 -0500

    Cederic Noel : sprm files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'clients/new'
+
+  get 'clients/create'
+
+  get 'clients/update'
+
+  get 'clients/edit'
+
+  get 'clients/destroy'
+
+  get 'clients/index'
+
+  get 'clients/show'
+
 resources :projects
   devise_for :users
   get "/pages/*id" => 'pages#show', as: :page, format: false
